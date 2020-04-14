7a203b36d9bd27fe24da65a833532defdca881db
Felicià MAVIANE <maviane@gmail.com>
2020-04-14 15:17:37 +0200

    Splitted betwin system and shell.\nDo not launch first script (build_linux.sh) with sudo

--- build_linux.sh
+++ build_linux.sh
@@ -1,51 +1,45 @@
 #!/bin/bash
 
-echo updating and upgrading
-sudo apt-get update && sudo apt-get upgrade
-
-echo First step
+echo First things first
 export RUNLEVEL=3
 sudo /etc/init.d/dbus start 
-
-echo Switching shell
-chsh -s /bin/zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sudo apt install powerline
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-
-echo Installing the essentials
-sudo apt-get install -y fortune
-sudo apt install fortunes-off
-sudo apt-get install sl
-sudo apt-get htop
+sudo apt-get update && sudo apt-get -y upgrade
+echo
 
 echo Getting graphic libraries
-sudo apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+sudo apt-get -y install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+
+echo install thingys
+sudo apt-get -y install fortune
+sudo apt -y install fortunes-off
+sudo apt-get -y install sl
+sudo apt-get -y htop
+sudo apt -y install zsh
+echo
 
 echo installing window manager
-sudo apt-get install xfce4
-sudo apt-get install xfce4-terminal
-sudo apt-get install xfce4-goodies
-sudo apt install xubuntu-desktop
+sudo apt-get -y install xfce4
+sudo apt-get -y install xfce4-terminal
+sudo apt-get -y install xfce4-goodies
+sudo apt -y install xubuntu-desktop
+echo
 
 echo Getting NPM
-sudo apt-get install npm
+sudo apt-get -y install npm
+echo
 
 echo Getting darktable
-sudo apt-get install darktable
+sudo apt-get -y install darktable
+echo
 
 echo Setting git ...
 git config --global user.email maviane@gmail.com
 git config --global user.name "Felicià MAVIANE"
 # git config --global credential.helper store
 git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/libexec/git-core/git-credential-manager.exe"
+echo
 
-echo Copy configuration files
-touch ~/.Xdefaults
-cp ~/build_linux/.p10k.zsh ~/
-cp ~/build_linux/.zshrc ~/
-cp ~/build_linux/.xinitrc ~/
+echo Switching shell
+chsh -s /bin/zsh
 
 echo Done
---
+++ init_shell.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo install powerline
+sudo apt -y install powerline
+echo
+
+echo Install oh my zosh and sugar
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo
+
+echo Copy configuration files
+touch ~/.Xdefaults
+cp ~/build_linux/.p10k.zsh ~/
+cp ~/build_linux/.zshrc ~/
+cp ~/build_linux/.xinitrc ~/
+echo
+
+echo Done
