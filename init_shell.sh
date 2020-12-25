70466afaf82240368804442967d96d2f56ecd922
Felicià MAVIANE <maviane@gmail.com>
2020-12-25 20:08:56 +0100

    Added some oh-my-zsh pluggins

--- init_shell.sh
+++ init_shell.sh
@@ -8,6 +8,8 @@ echo Install oh my zosh and sugar
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 echo
 
 echo Copy configuration files
